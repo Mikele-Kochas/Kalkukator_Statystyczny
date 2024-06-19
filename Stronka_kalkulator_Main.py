a63958324a990588b97243695142f8f7a4aba689
Mikele-Kochas <michalkocher24@gmail.com>
2024-06-19 21:17:18 +0200

    Add files via upload

---
+++ Stronka_kalkulator_Main.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.set_page_config(page_title="Kalkulator Statystyczny")
+st.subheader("Siema! Uczę się programowania i statystki. Żeby się sprawdzić, napisałem stronkę z wbudowanym kalkulatorem statystycznym.  ")
